73c6e8e330b0960ebbf7567250a315e3bf6820f5
Nitish-KumarSP <f20250730@pilani.bits-pilani.ac.in>
2026-08-28 05:01:47 +0000

    lab01 task2

--- labs/lab01/task2/FA_Gate.v
+++ labs/lab01/task2/FA_Gate.v
@@ -29,7 +29,7 @@ module FA_Gate(
   xor #(2,3) (ps,  a,   b);
   and #(2,3) (pc1, a,   b);
   xor #(2,3) (sum, cin, ps);
-  and #(2,3) (pc2, cin, ps);
-  or  #(2,3) (cout, pc1, pc2);
+  and #(3,2) (pc2, cin, ps);
+  or  #(2,2) (cout, pc1, pc2);
 
 endmodule
